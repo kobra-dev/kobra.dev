72a42c8348ce7d1bb37436a2f1469025cf42a2b7
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-10 15:36:55 +0200

    updated features

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,5 +1,6 @@
 import {
     Box,
+    Center,
     Divider,
     Flex,
     Heading,
@@ -47,12 +48,41 @@ export default function Features(props: FeaturesProps) {
                         {props.features.map((feature) => (
                             <WrapItem
                                 key={feature.heading + feature.description}
-                                borderWidth="1px"
-                                p={4}
+                                borderWidth="0px"
+                                p={8}
                                 w="400px"
                             >
-                                <Box>
-                                    {feature.icon}
+                                <Box
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        gap: '2rem'
+                                    }}
+                                >
+                                    <Center
+                                        style={{
+                                            backgroundColor: '#effff4',
+                                            width: '4rem',
+                                            padding: '20px 10px',
+                                            borderRadius: '50%'
+                                        }}
+                                    >
+                                        {/* {feature.icon} */}
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            width="24"
+                                            height="24"
+                                            fill="#68d290"
+                                        >
+                                            <path
+                                                fill="none"
+                                                d="M0 0h24v24H0z"
+                                            />
+                                            <path d="M6.923 21C5.113 18.664 4 15.493 4 12c0-3.493 1.113-6.664 2.923-9h2.014C7.235 5.388 6.2 8.542 6.2 12s1.035 6.612 2.737 9H6.923zm10.151 0H15.06c1.702-2.388 2.737-5.542 2.737-9s-1.035-6.612-2.737-9h2.014c1.81 2.336 2.923 5.507 2.923 9 0 3.493-1.112 6.664-2.923 9z" />
+                                        </svg>
+                                    </Center>
                                     <Heading as="h3" size="lg">
                                         {feature.heading}
                                     </Heading>
