19bedd8ba3d415c748c5c29fc7dd6abb78e3a850
Pranav Teegavarapu <pranavnt@outlook.com>
2021-05-12 13:20:14 -0700

    add analytics (@plausible)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import "../styles/globals.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import '../styles/globals.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
-import { useEffect } from "react";
+import PlausibleProvider from 'next-plausible';
+import { useEffect } from 'react';
 
 /*import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -11,30 +12,32 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas);*/
 
 const theme = extendTheme({
-  colors: {
-    // https://smart-swatch.netlify.app/#00d777
-    brand: {
-      50: '#dafff1',
-      100: '#adffdb',
-      200: '#7cffc5',
-      300: '#4affae',
-      400: '#1aff99',
-      //500: '#00e67f',
-      500: '#00d777',
-      600: '#00b362',
-      700: '#008046',
-      800: '#004e28',
-      900: '#001c09',
+    colors: {
+        // https://smart-swatch.netlify.app/#00d777
+        brand: {
+            50: '#dafff1',
+            100: '#adffdb',
+            200: '#7cffc5',
+            300: '#4affae',
+            400: '#1aff99',
+            //500: '#00e67f',
+            500: '#00d777',
+            600: '#00b362',
+            700: '#008046',
+            800: '#004e28',
+            900: '#001c09'
+        }
     }
-  }
-})
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+    return (
+        <PlausibleProvider domain="kobra.dev">
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </PlausibleProvider>
+    );
 }
 
 export default MyApp;
